cc2f8647d6e399b7374aef4bf175f7750044b52c
iter1pina <iter1pina@gmail.com>
2024-05-05 17:19:14 -0100

    scroll

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import Link from 'next/link'
-import React, { Component } from 'react'
-import { Popover } from '@headlessui/react'
+import React, { Component, Fragment } from 'react'
+import { Popover, Transition } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/16/solid'
 //import {Bars3Icon} from "@heroicons/react"
 
@@ -28,6 +28,18 @@ export class home extends Component {
         <Bars3Icon className='h-6 w-6' aria-hidden="true"/>
         </Popover.Button>
         </div>
+        <Popover.Overlay className="sm:hidden fixed inset-0 bg-black opacity-30" />
+        <Transition
+        as={Fragment}
+        enter='duration=200 ease-out'
+        enterFrom='opacity-0 scale-95'
+        enterTo='opacity-100 scale-100'
+        leave='duaration-100 ease-in'
+        leaveFrom='opacity-100 scale-100'
+        leaveTo='opacity-0 scale-95'
+        >
+
+        
 
         <Popover.Panel
         focus
@@ -36,7 +48,11 @@ export class home extends Component {
           <div className='rounded-lg bg-white shadow-lg ring-1 ring-black ring-opacity-5 divide-y-2 divide-gray-50'>
             <div className='px-5 pt-5 pb-6'>
               <div className='flex items-center justify-between'>
+                <div className='flex gap-1 mt-1'>
+                <img src="/img/usLogo.jpg" alt="logo" width={50} height={50} />
+
                 <h1 className='font-bold'>US Virtual</h1>
+                </div>
                 <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
                   <span className='sr-only'>Close Menu</span>
                   <XMarkIcon className='h-6 w-6' aria-hidden="true" />
@@ -45,14 +61,39 @@ export class home extends Component {
               </div>
 
             </div>
+            <div className='mt-6'>
+              <nav className='grid gap-y-8'>
+              
+                <Link href="#" className='focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2 border-1 py-4'>Home</Link>
+                <Link href="#" className='focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2 border-1 py-4'>Blog</Link>
+                <Link href="#"  className='focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2 border-1 py-4'>Academia</Link>
+                <Link href="#"  className='focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2 border-1 py-4'>secretaria</Link>
+                <Link href="#"  className='focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500 px-2 border-1 py-4'>Sobre</Link>
+           
+
+              </nav>
+
+              <div className='mt-6 flex flex-col items-center gap-2'>
+                <Link href="#"
+                className='rounded-md bg-white px-4 py-2 text-sm font-mediun text-black md:text-xl w-full border-2
+                focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-500'
+                >
+                  Sign Up
+                </Link>
+
+              </div>
+
+            </div>
           </div>
         </Popover.Panel>  
+
+        </Transition>
 
         <div className='hidden sm:block'>
             <Link href="#" className='mr-2 font-bold'>Sign up</Link>
             <Link href="#" className='font-bold'>Login</Link>
         </div>
-
+        
       </Popover>
     )
   }
---
+++ app/secretaria/page.tsx
@@ -0,0 +1,138 @@
+"use client"
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { Component } from 'react'
+
+export class secretaria extends Component {
+  render() {
+    return (
+
+      <div className='max-w-screen-md mx-auto h-1/3 items-center justify-between flex gap-8 overflow-y-scroll'>
+        
+
+        
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52 shadow-sm hover:bg-gray-200'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declarações normalizadas para efeitos profissionais, fiscais,
+             pedidos de bolsas e crédito</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declarações com conteúdo específico</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declarações para justificação de faltas</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declarações para reingresso ou mudança de curso</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declarações para anulação de matrícula</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+       
+        
+
+       
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declarações para trancamento do curso ou ano/suspensão de frequência</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declarações de participação em atividades e eventos académicos</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declarações de inscrição, frequência, exame ou aprovação ou matrícula</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Certidões de notas discriminadas das disciplinas</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Certidão de Frequência</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+      
+
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Declaração de Aproveitamento</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Certificado de Conclusão de Curso </span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Diploma</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Carta de curso de Licenciatura e de Mestrado</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Certidões de conclusão de cursos não conferentes de grau</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+       
+
+        <Link href="#" className='scroll-ml-6 snap-start'>
+        <div  className='mt-4 ml-2 container px-4 items-center justify-center rounded-sm border-inherit border-2 w-52'>
+            <span className='items-center justify-center max-w-fit font-semibold text-xl'>Suplemento ao Diploma</span>
+            <Image src="/img/pdf1.png" width={150} height={150} alt='pdf' />
+        </div>
+        </Link>
+      </div>
+    )
+  }
+}
+
+export default secretaria
